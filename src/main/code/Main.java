c39cb799746d091e63ae25ef52045981cab84867
TheBigBagrovski <matvey.bagr77@mail.ru>
2022-10-29 20:26:22 +0300

    Absolute paths bug fixed

--- src/main/code/Main.java
+++ src/main/code/Main.java
@@ -13,12 +13,10 @@ import java.nio.file.Files;
 import java.nio.file.NoSuchFileException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.rmi.ServerError;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
-//TODO() фикс бага с абсолютными путями
 //TODO() тесты
 //TODO() рефакторинг
 //TODO() тесты
@@ -26,6 +24,7 @@ import java.util.zip.ZipOutputStream;
 //todo() slipProtect
 //TODO() тесты
 //TODO() описания функций в javadoc стиле, рефакторинг, тесты
+
 /**
  * Options:
  * -u - unzip file in files instead of zipping
@@ -46,7 +45,7 @@ public class Main {
     @Option(name = "-a")
     private boolean a;
     @Argument(required = true)
-    private List<String> userInputs;
+    private List<String> userInput;
 
     private String userPath = "";
     private String userArchiveName;
@@ -68,7 +67,7 @@ public class Main {
             setUserArchiveName();
             setUserPath();
             zipper();
-        } catch (IOException e) {
+        } catch (Exception e) {
             System.err.println("Exception while archiving\n" + e.getMessage());
             printFail();
             return;
@@ -86,9 +85,9 @@ public class Main {
 
     private void setUserArchiveName() {
         if (!isArchiveNameValid()) throw new IllegalArgumentException("Invalid archive name: " +
-                userInputs.get(userInputs.size() - 1) + ".zip");
-        userArchiveName = userInputs.get(userInputs.size() - 1) + ".zip";
-        userInputs.remove(userInputs.size() - 1);
+                userInput.get(userInput.size() - 1) + ".zip");
+        userArchiveName = userInput.get(userInput.size() - 1) + ".zip";
+        userInput.remove(userInput.size() - 1);
         System.err.println("Archive name set: " + userArchiveName);
     }
 
@@ -104,7 +103,7 @@ public class Main {
     }
 
     private boolean isArchiveNameValid() {
-        String str = userInputs.get(userInputs.size() - 1);
+        String str = userInput.get(userInput.size() - 1);
         if (str == null || str.isEmpty() || str.length() > 255) {
             return false;
         }
@@ -116,31 +115,31 @@ public class Main {
 
     private void setUserPath() {
         if (p) {
-            userPath = userInputs.get(userInputs.size() - 1);
+            userPath = userInput.get(userInput.size() - 1);
             if (userPath.charAt(userPath.length() - 1) != '\\') userPath += '\\';
             if (!Files.exists(Paths.get(userPath))) throw new IllegalArgumentException("Path not found: " + userPath);
-            userInputs.remove(userInputs.size() - 1);
+            userInput.remove(userInput.size() - 1);
             System.err.println("Path set: " + userPath);
         } else {
             System.err.println("Archive will be created in current working directory");
         }
     }
 
-    private void zipper() throws IOException {
+    private void zipper() throws IOException, IllegalArgumentException {
         if (!u) {
             List<File> filesToZip = new ArrayList<>();
-            List<String> fileNamesToZip = new ArrayList<>();
             if (a) {
-                if (!userInputs.isEmpty()) throw new IllegalArgumentException("Wrong input: -a with files");
+                if (!userInput.isEmpty()) throw new IllegalArgumentException("Wrong input: -a with files");
                 getFilesInCurrentDir(filesToZip);
             } else {
-                getExistingFiles(filesToZip);
-            }
-            for (File file : filesToZip) {
-                generateFileList(fileNamesToZip, file);
+                if (userInput.isEmpty()) throw new IllegalArgumentException("Wrong input: no files provided");
+                getExistingFilesFromUserInput(filesToZip);
             }
             System.err.println("Total file size: " + getFilesSize(filesToZip) / 1000 + " kB");
-            zip(fileNamesToZip);
+            List<String> absPaths = new ArrayList<>();
+            List<String> correctNames = new ArrayList<>();
+            getListsOfNames(filesToZip, absPaths, correctNames);
+            zip(absPaths, correctNames);
             System.err.println("Archive size: " + new File(userPath + userArchiveName).length() / 1000 + " kB");
         }
     }
@@ -160,31 +159,47 @@ public class Main {
             if (!file.exists()) throw new NoSuchFileException("No such file: " + userInput);
             else return file;
         } catch (IOException e) {
-            throw new IOException("Exception during searching files", e);
+            throw new IOException("Exception while searching files: " + userInput + "\n" + e.getMessage(), e);
         }
     }
 
-    private void getExistingFiles(List<File> filesToZip) throws IOException {
-        for (String userInput : userInputs) {
+    private void getExistingFilesFromUserInput(List<File> filesToZip) throws IOException {
+        for (String userInput : userInput) {
             filesToZip.add(getExistingFile(userInput));
         }
     }
 
-    private void zip(List<String> filesToZip) throws IOException {
+    private void getListsOfNames(List<File> listOfFiles, List<String> absPaths, List<String> correctNames) throws IOException {
+        for (File file : listOfFiles) {
+            String common = file.getAbsolutePath().substring(0, file.getAbsolutePath().lastIndexOf('\\') + 1);
+            List<String> temp = new ArrayList<>();
+            generateFileList(temp, file);// returns abs paths for files (including those in folders)
+            absPaths.addAll(temp);
+            for (String absPath : temp) {
+                String str = absPath.replace(common, "");
+                if (str.startsWith(".\\")) str = str.substring(2); // removes "./" from file name
+                correctNames.add(str);
+            }
+        }
+    }
+
+    private void zip(List<String> absPaths, List<String> correctNames) throws IOException {
         byte[] bytes;
         int length;
         ZipEntry zipEntry;
         FileInputStream fis;
         try (FileOutputStream fos = new FileOutputStream(userPath + userArchiveName);
              ZipOutputStream zos = new ZipOutputStream(fos)) {
-            for (String fileToZip : filesToZip) {
-                if (fileToZip.startsWith(".\\")) fileToZip = fileToZip.substring(2); // removes "./" from file name
-                if (fileToZip.equals(userArchiveName)) continue; // skip .zip itself when -a
-                System.err.println("Zipping file: " + fileToZip);
-                zipEntry = new ZipEntry(fileToZip);
+            for (int i = 0; i < correctNames.size(); i++) {
+                String fileName = correctNames.get(i);
+                String absPath = absPaths.get(i);
+                if (fileName.equals(userArchiveName)) continue; // skip .zip itself when -a
+                System.err.println("Zipping file: " + fileName);
+                zipEntry = new ZipEntry(fileName);
+                System.err.println(zipEntry);
                 zos.putNextEntry(zipEntry);
-                if (new File(fileToZip).isDirectory()) continue; // for empty folder
-                fis = new FileInputStream(fileToZip);
+                if (new File(absPath).isDirectory()) continue; // if folder => empty folder => nothing to write
+                fis = new FileInputStream(absPath);
                 bytes = new byte[1024];
                 while ((length = fis.read(bytes)) >= 0) {
                     zos.write(bytes, 0, length);
@@ -192,7 +207,7 @@ public class Main {
                 fis.close();
             }
         } catch (IOException e) {
-            throw new IOException("Exception while adding files to zip", e);
+            throw new IOException("Exception while adding files to zip:\n" + e.getMessage(), e);
         }
     }
 
@@ -217,13 +232,13 @@ public class Main {
     private boolean isDirectoryEmpty(File directory) throws IOException {
         String[] files = directory.list();
         if (files != null) return files.length == 0;
-        else throw new IOException("Exception while getting empty folder");
+        else throw new IOException("Exception while getting empty folder: " + directory);
     }
 
     private long getFilesSize(List<File> files) throws IOException {
         long sum = 0;
         for (File file : files) {
-            if(file.isDirectory()) {
+            if (file.isDirectory()) {
                 sum += Files.walk(Path.of(file.getAbsolutePath()))
                         .filter(p -> p.toFile().isFile())
                         .mapToLong(p -> p.toFile().length())
